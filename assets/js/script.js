1016b32839e71342e91a960525bc503c8af020f2
LNDavid <ldavid@rmacan.com>
2022-03-28 15:47:30 -0400

    populate rows with time/textarea/save button combination

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,58 @@
 // use Moment.js to display current date in a div, with help from Stackoverflow 
-var datetime = null;
+var currentTime = null;
 var date = null;
 
 var update = function () {
    date = moment(new Date())
-   datetime.html(date.format('DD MMMM YYYY, h:mm:ss a'));
+   currentTime.html(date.format('DD MMMM YYYY, HH:mm:ss'));
 };
 
 $(document).ready(function() {
-   datetime = $("#currentDay")
+   currentTime = $("#currentDay")
    update();
    setInterval(update, 1000);
+
+
+var apptTime = ["8:00", "9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"];
+
+// create array from values in hour (var apptTime) & description classes on click of saveBtn
+
+
+// when save button clicked, input to textarea (classname "description" )
+$(".description").on("click", "textarea", function() {
+   var apptDetails = $(this).apptDetails().trim();
+   var textInput = $("<textarea>").addClass("description").val(apptDetails); 
 });
 
-// create array of time block values to use for local storage options
 
-//var timeBlockHour = ["8:00", "9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"];
-var Index = 10;
-
-// clicking save button sends appointment to local storage - index using id reference for each hour/details combination
-// need to create variable representing the appointment input  
-var createAppointment = $("saveBtn").on("click", function() {
-var appointment = "";
-
-for (var i = 0; i < Index; i++) {
-   var apptTime = $("#hour[i]").val();
-   var apptDetails = $("#detail[i]").val()
-   appointment.push({
-      time: apptTime,
-      text: apptDetails
-   });
-  console.log(appointment);
+   // create array of time block values to use for local storage options; hour (apptTime) and description (apptDetails) are children of row time-block.
   
-  i=i++
-}
-});
 
-createAppointment();
-
-//based on current time, determine if appointment is already past (red) or future(green)
-function compareTime() {
-
-   var currentHour = moment().format("hh a");
-   console.log(currentHour);
-   
-   //if hour < today, red; if hour is > today, green, else default
-   $(".time-block").each(function() {
-      var apptTime = parseInt($(this).attr("id"))
-
-      if (apptTime < currentHour) {
-        $(this).addClass("past");// time slot takes on ".past" class
-      } 
+   //based on current time, determine if appointment is already past (red) or future(green)
+   var currentHour = (date.format('HH:mm'));
       
-      else if (apptTime > currentHour) {  
-        $(this).addClass("future");// time slot takes on .future class
-      }
-      else {
-      $(this).addClass("present");
-   }  
+   //index each hour appointent slot
+   jQuery.each(apptTime, function(index,value) {
+      console.log("index", index, "value", value);
+   });
+   console.log(apptTime);
 
-datetime.trigger("focus");   
+   // if hour < currenttoday, red; if hour is > today, green, else default
+  for(var i = 0; i < apptTime.length;) {
+      $(".time-block").each(function() {
+         var apptTime = parseInt($(this).attr("id"))
 
-  })}
+         if (apptTime === currentHour) {
+            $(this).addClass("present");
+         } 
+         else if (apptTime > currentHour) 
+         {  
+            $(this).addClass("future");// time slot takes on .future class
+         }
+         else {
+            $(this).addClass("past"); // time slot takes on ".past" class
+         }  
+      });
+      i++
+   }
+});
