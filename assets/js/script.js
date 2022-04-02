ac6e711c016ed9a67aed45b869f438141a39c2d1
LNDavid <ldavid@rmacan.com>
2022-04-02 10:31:39 -0400

    establish this connection to p elements for time comparison

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,47 +44,50 @@ $(document).ready(function() {
          }  
       } 
    });  
-      // if hour attached to each time block is earlier than current time (by hour), background is red, if later, background is green, otherwise grey
-        var textDetails = JSON.parse(window.localStorage.getItem("apptEl")); 
-        console.log(textDetails);
+      // // if hour attached to each time block is earlier than current time (by hour), background is red, if later, background is green, otherwise grey
+      //   var textDetails = JSON.parse(window.localStorage.getItem("apptEl")); 
+      //   console.log(textDetails);
+
+        
 
       // create an array of all hours in the schedule
-         var hours = ["08:00", "09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"];              
+         var hours = parseInt(["08:00", "09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"]);              
          console.log(hours.length);
          // get current time, in hours, when site is being viewed for comparison
-         var currentHour = moment().hour()
+         var currentHour = parseInt(moment().hour())
          console.log(currentHour)
          
-         function timeBlockStatus() {
-            $(".description").each(function() {
+         function timeBlockCompare() {
+            $("p").each(function() {
                console.log(this)
-               var timeBlockRow = parseInt($(this).attr("id"))
-               if (currentHour === timeBlockRow) {
-                  $(this).addClass("present")
-               }
-               else if (currentHour < timeBlockRow) {
-                  $(this).removeClass("past")
-               }
-               else (currentHour > timeBlockRow) ;{
-                  $(this).removeClass("future")
-               }
-            })
+               for (var i = 0; i < hours.length; i++ ) {
+                  if (hours[i] < currentHour) {
+                     $(textDetails).addClass("past");
+                  }
+                  else if (hours > currentHour) {
+                     $(textDetails).addClass("future");
+                  }
+                  else { 
+                     $(textDetails).addClass("present");
+                              }
+                  console.log(hours[i]);
+              
+               
+            }
+         //       if (currentHour === timeBlockRow) {
+         //          $(this).addClass("present")
+         //       }
+         //       else if (currentHour < timeBlockRow) {
+         //          $(this).removeClass("past")
+         //       }
+         //       else (currentHour > timeBlockRow) ;{
+         //          $(this).removeClass("future")
+         //       }
+            });
          }
-         timeBlockStatus();
-         // for (var i = 0; i < hours.length; i++ ) {
-         //    if (hours[i] < currentHour) {
-         //       $(textDetails).addClass("past");
-         //    }
-         //    else if (hours > currentHour) {
-         //       $(textDetails).addClass("future");
-         //    }
-         //    else { 
-         //       $(textDetails).addClass("present");
-         //                }
-         //    console.log(hours[i]);
-         // }
-             
-       
+        
+         
+         timeBlockCompare();
 
 
 }); // currentDay
